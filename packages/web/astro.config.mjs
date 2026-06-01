344c491504cba8c563a46e18778f20486bd058e9
Lucky Ivanius <luckzivanius@gmail.com>
2026-06-02 02:16:02 +0700

    chore: remove react

--- packages/web/astro.config.mjs
+++ packages/web/astro.config.mjs
@@ -1,22 +1,16 @@
 // @ts-check
 
 import cloudflare from "@astrojs/cloudflare";
-import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
-
   vite: {
-    plugins: [
-      {
-        ...tailwindcss(),
-        name: "tailwindcss",
-      },
-    ],
+    resolve: {
+      tsconfigPaths: true,
+    },
+    plugins: [tailwindcss()],
   },
-
-  integrations: [react()],
 });
